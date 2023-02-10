1fe6ea32c040936fcf21395b90c93ebb787cb501
Haejin Hwang <hhwang24@my.bcit.ca>
2023-02-09 17:43:19 -0800

    Initial commit

---
+++ madlibs_1.py
@@ -0,0 +1,20 @@
+#2
+my_fav_animal = "cat"
+user_fav_animal = input("your fav animal?")
+print(my_fav_animal==user_fav_animal.lower())
+
+# month = 4
+# age = 16
+# can_get_license = month >= 6 and age >= 16
+# print(can_get_license)
+
+# casting input type is string
+# assignment vs comparison
+
+# animal = "mouse"
+# #animal = "mouse" or "cat"
+# #symentic error
+# animal = "mouse" or animal = "cat"
+# print(animal)
+
+#composite expression
